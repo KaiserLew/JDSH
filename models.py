e9e94265cfb8c6ad703890fcd1d4c2626dce31ba
liusong <Ls_0626@126.com>
2020-12-08 18:46:02 +0800

    Update models.py

--- models.py
+++ models.py
@@ -5,25 +5,24 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
-
 class ImgNet(nn.Module):
     def __init__(self, code_len):
         super(ImgNet, self).__init__()
         self.alexnet = torchvision.models.alexnet(pretrained=True)
         self.alexnet.classifier = nn.Sequential(*list(self.alexnet.classifier.children())[:6])
-        self.fc_encode = nn.Linear(4096, code_len)
+        self.hash_layer = nn.Linear(4096, code_len)
         self.alpha = 1.0
 
     def forward(self, x):
 
-        #with torch.no_grad():
-        x = self.alexnet.features(x)
-        x = x.view(x.size(0), -1)
-        feat = self.alexnet.classifier(x)
+        with torch.no_grad():
+            x = self.alexnet.features(x)
+            x = x.view(x.size(0), -1)
+            feat = self.alexnet.classifier(x)
 
-        hid = self.fc_encode(feat)
-        code = F.tanh(self.alpha * hid)
+        hid = self.hash_layer(feat)
+        feat = F.normalize(feat, dim=1)
+        code = torch.tanh(self.alpha * hid)
 
         return feat, hid, code
 
@@ -34,15 +33,18 @@ class ImgNet(nn.Module):
 class TxtNet(nn.Module):
     def __init__(self, code_len, txt_feat_len):
         super(TxtNet, self).__init__()
-        self.fc1 = nn.Linear(txt_feat_len, 4096)
-        self.fc2 = nn.Linear(4096, code_len)
+
+        self.net = nn.Sequential(nn.Linear(txt_feat_len, 4096),
+                                 nn.ReLU(inplace=True),
+                                 nn.Linear(4096, code_len),
+                                 )
+
         self.alpha = 1.0
 
     def forward(self, x):
-        feat = F.relu(self.fc1(x))
-        hid = self.fc2(feat)
-        code = F.tanh(self.alpha * hid)
-        return feat, hid, code
+        hid = self.net(x)
+        code = torch.tanh(self.alpha * hid)
+        return code
 
     def set_alpha(self, epoch):
         self.alpha = math.pow((1.0 * epoch + 1.0), 0.5)
